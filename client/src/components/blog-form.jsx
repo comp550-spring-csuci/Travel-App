8236fd7de2bf179301530c4a51d9a01a80e373f3
arieldonner <arieldonner@gmail.com>
2025-05-03 11:27:48 -0700

    Changed thumbnail url to file upload

--- client/src/components/blog-form.jsx
+++ client/src/components/blog-form.jsx
@@ -10,6 +10,7 @@ export default class AddBlog extends React.Component {
             title: '',
             content: '',
             image: '',
+            file: null,
             location: '',
             latitude: '',
             longitude: '',
@@ -31,35 +32,32 @@ export default class AddBlog extends React.Component {
         const {title, content, image, location, latitude, longitude} = this.state;
         const {user, token} = this.context;
 
+        const form = new FormData();
+        form.append("title", title);
+        form.append("content", content);
+        form.append("location", location);
+        form.append("latitude", latitude);
+        form.append("image", file);
+        form.append("author", user.id);
+
         fetch('/api/post/newblog', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
                 'x-access-token': token
             },
-            body: JSON.stringify({
-                title, 
-                content, 
-                image, 
-                location, 
-                latitude:parseFloat(latitude),
-                longitude: parseFloat(longitude),
-                author: user.id
-            })
+            body: form
         })
             .then(res => {
-                if (!res.ok) throw new Error(`${res.status}`);
+                if (!res.ok) throw new Error(res.status);
                 return res.json();
             })
             .then(() => {
                 window.location.hash = '#blog-feed';
             })
-        // const req = {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(this.state)
+            .catch(err => {
+                console.error("Upload failed", err);
+                this.setState({error: true, loading: false})
+            })
         };
         //this.setState({ invalid: true });
         //if signing in and bad response -> set to incorrect, if signing up -> redirect to sign in, if user and has token -> sign in
@@ -162,14 +160,22 @@ export default class AddBlog extends React.Component {
                             className='form-control'/>
                         </div>
                         <div className='mb-4'>
-                            <label htmlFor='thumbnail' className='form-label'>Thumbnail</label>
-                            <input 
-                            id='thumbnail'
+                            <label htmlFor='image' className='form-label'>Upload Image</label>
+                            {/* <input 
+                            id='image'
                             type='url'
                             name='image'
                             value={image}
                             onChange={handleChange}
-                            className='form-control'/>
+                            className='form-control'/> */}
+                            <input 
+                            id="image"
+                            name="image"
+                            type="file"
+                            accept="image/*"
+                            onChange={event => this.setState({file: event.target.files[0]})}
+                            className="form-control"
+                            />
                         </div>
                         {this.state.incorrect === true &&
                             <div>
